4d0bde72f214509995947307b5bf8ce079d0b270
ronanlima <ronan.saleslima@gmail.com>
2017-02-13 18:30:48 -0200

    Iniciando com o FoldingCell

--- app/src/main/java/br/beer/beerbeacon/MainActivity.java
+++ app/src/main/java/br/beer/beerbeacon/MainActivity.java
@@ -9,6 +9,8 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.ramotion.foldingcell.FoldingCell;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -18,12 +20,20 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-       FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+            }
+        });
+
+        final FoldingCell fc = (FoldingCell) findViewById(R.id.folding_cell);
+        fc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                fc.toggle(false);
             }
         });
     }
